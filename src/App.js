e9072edcd7bc30bfe0bf7845bfb51f2e83c71e16
asraful196 <asraful.01110@gmail.com>
2022-12-07 18:03:01 +0600

    update some code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import "./App.css";
-
 import { useForm } from "react-hook-form";
+import React, { useState } from "react";
+import "./App.css";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    // height:"100px",
+    width:"400px",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "lightgray",
+  },
+};
 
 function App() {
+  const [data, setData] = useState("");
   const {
     register,
     handleSubmit,
@@ -13,26 +29,53 @@ function App() {
 
   const onSubmit = (data) => {
     console.log(data);
+    setData(data);
     reset();
   };
-
 
   // console.log(watch());
 
   // console.log(errors.name)
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = useState(false);
 
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    subtitle.style.color = "#fff";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   return (
-    <div className="container pt-5">
+    <div className="container pt-5 ">
       <div className="row justify-content-sm-center pt-5">
-        <div className="col-sm-6 shadow round pb-3">
-          <h1 className="text-center pt-3 text-secondary">Example Form</h1>
+        <div className="col-sm-6 shadow rounded pb-3 bg-info  ">
+          <h1 className="text-center pt-3 text-white">Contact Form</h1>
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="form-group">
               <label className="col-form-label">Name:</label>
               <input
                 type="text"
                 className={`form-control ${errors.name && "invalid"}`}
-                {...register("name", { required: "Name is Required" })}
+                {...register("name", {
+                  required: "Name is Required",
+                  minLength: {
+                    value: 3,
+                    message: "Minimum Required length is 3",
+                  },
+                  maxLength: {
+                    value: 30,
+                    message: "Maximum allowed length is 30",
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z]+([._]?[a-zA-Z]+)*$/,
+                    message: "Only (a-z) are allowed",
+                  },
+                })}
                 onKeyUp={() => {
                   trigger("name");
                 }}
@@ -41,44 +84,19 @@ function App() {
                 <small className="text-danger">{errors.name.message}</small>
               )}
             </div>
-            <div className="form-group">
-              <label className="col-form-label">Age:</label>
-              <input
-                type="text"
-                className={`form-control ${errors.age && "invalid"}`}
-                {...register("age", {
-                  required: "Age is Required",
-                  min: {
-                    value: 13,
-                    message: "Minimum Required age is 13",
-                  },
-                  max: {
-                    value: 65,
-                    message: "Maximum allowed age is 65",
-                  },
-                  pattern: {
-                    value: /^[0-9]*$/,
-                    message: "Only numbers are allowed",
-                  }
-                })}
-                onKeyUp={() => {
-                  trigger("age");
-                }}
-              />
-              {errors.age && (
-                <small className="text-danger">{errors.age.message}</small>
-              )}
-            </div>
+
             <div className="form-group">
               <label className="col-form-label">Email:</label>
               <input
                 type="text"
                 className={`form-control ${errors.email && "invalid"}`}
-                {...register("email", { required: "Email is Required" ,
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: "Invalid email address",
-                }})}
+                {...register("email", {
+                  required: "Email is Required",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "Invalid email address",
+                  },
+                })}
                 onKeyUp={() => {
                   trigger("email");
                 }}
@@ -92,15 +110,17 @@ function App() {
               <input
                 type="text"
                 className={`form-control ${errors.phone && "invalid"}`}
-                {...register("phone", { required: "Phone is Required",
-                pattern: {
-                  value: /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
-                  message: "Invalid phone no",
-                },
-               })}
-               onKeyUp={() => {
-                trigger("phone");
-              }}
+                {...register("phone", {
+                  required: "Phone is Required",
+                  pattern: {
+                    value:
+                      /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
+                    message: "Invalid phone no",
+                  },
+                })}
+                onKeyUp={() => {
+                  trigger("phone");
+                }}
               />
               {errors.phone && (
                 <small className="text-danger">{errors.phone.message}</small>
@@ -110,31 +130,64 @@ function App() {
               <label className="col-form-label">Message:</label>
               <textarea
                 className={`form-control ${errors.message && "invalid"}`}
-                {...register("message", { required: "Message is Required",
-                minLength: {
-                  value: 10,
-                  message: "Minimum Required length is 10",
-                },
-                maxLength: {
-                  value: 50,
-                  message: "Maximum allowed length is 50 ",
-                }
-               })}
-               onKeyUp={() => {
-                trigger("message");
-              }}
+                {...register("message", {
+                  required: "Message is Required",
+                  minLength: {
+                    value: 0,
+                    message: "Minimum Required length is 0",
+                  },
+                  maxLength: {
+                    value: 80,
+                    message: "Maximum allowed length is 80 ",
+                  },
+                })}
+                onKeyUp={() => {
+                  trigger("message");
+                }}
               ></textarea>
               {errors.message && (
                 <small className="text-danger">{errors.message.message}</small>
               )}
             </div>
-            <input
-              type="submit"
-              className="btn btn-primary my-3"
-              value="Send message"
-            />
+            <div className="d-flex justify-content-center">
+              <input
+                onClick={openModal}
+                type="submit"
+                className="btn  btn-primary my-3"
+                value="Send message"
+              />
+            </div>
           </form>
         </div>
+      </div>
+
+      <div>
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          {data.name ? (
+            <div>
+              <h1>Thank you {data.name}</h1>{" "}
+              <button className="btn btn-danger" onClick={closeModal}>
+                close
+              </button>
+            </div>
+          ) : (
+            <div>
+              <strong className="text-danger">
+                Please fill the required fields
+              </strong>
+              <br />
+              <button className="btn btn-danger" onClick={closeModal}>
+                close
+              </button>
+            </div>
+          )}
+        </Modal>
       </div>
     </div>
   );
